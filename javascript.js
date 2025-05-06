678c09ea385b123c7c99b2862a2ea613a0254fc4
tarek <tarekelshenawy1997@gmail.com>
2025-05-06 19:35:41 +0300

    solar company

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let nav_list = document.querySelector(".nav-list");
+let nav_contact = document.querySelector(".nav_contact");
+
+
+function handlemenuicon(){
+    nav_list.classList.toggle("active");
+    nav_contact.classList.toggle("active");
+    
+}
